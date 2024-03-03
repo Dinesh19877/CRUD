b23b8d5744ed8d53e68140c094cb27c1c45a3027
DINESH SHARMA <119472084+Dinesh19877@users.noreply.github.com>
2024-03-03 13:07:22 +0545

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,18 @@
+<=====for creating database===>
+CREATE database crud;
+
+
+
+<====to create register table =======>
+CREATE TABLE crud2 (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+     email varchar(255) NOT NULL,
+     number varchar(255) NOT NULL,
+     password varchar(255) NOT NULL,
+     file varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+
